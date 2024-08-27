6f7b03430c70297693eac3a79d7d2139aca3790a
jctaoo <jctaoo@outlook.com>
2024-08-27 14:16:15 +0800

    done wechat login page

--- src/renderer/pages/WechatLoginPage.vue
+++ src/renderer/pages/WechatLoginPage.vue
@@ -2,19 +2,22 @@
 import { WINDOW_PATH } from "@common/path";
 import LinkButton from "@renderer/components/LinkButton.vue";
 import AuthLayout from "@renderer/layouts/AuthLayout.vue";
-import { NFlex, NButton, NTabs, NTabPane, NForm, NFormItemRow, NInput, NDivider } from "naive-ui"
+import { NFlex, NButton, NTabs, NTabPane, NForm, NFormItemRow, NInput, NDivider, NQrCode } from "naive-ui"
 import { useRouter } from "vue-router";
 
 const login = async () => {
-  console.log('login');
-  await window.electronAPI.storeLoginToken('token');
+    console.log('login');
+    await window.electronAPI.storeLoginToken('token');
 };
 </script>
 
 <template>
-  <AuthLayout>
-  </AuthLayout>
+    <AuthLayout>
+        <NFlex vertical justify="center" align="center">
+            <n-qr-code value="text" :size="200" padding="0" />
+            <span class="mt-3 text-gray-500">使用微信扫一扫登录</span>
+        </NFlex>
+    </AuthLayout>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
